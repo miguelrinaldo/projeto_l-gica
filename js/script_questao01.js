6687940950854b6271767c3cb3075b19f5595c52
Miguel Rinaldo <miguel.r.nascimento9@aluno,senai.br>
2026-06-11 19:34:41 -0300

    adicionei vários comandos no script_questao01.js

--- js/script_questao01.js
+++ js/script_questao01.js
@@ -1,3 +1,20 @@
 const formDados = document.querySelector('#formulario')
 const divResultado = document.querySelector('#resultado')
 
+//CAPTURANDO O EVENTO DE SUBMIT
+formDados.addEventListener('submit' , (evt) =>  {
+
+const form_num = nem FormData(formDados)
+
+let num1 = parseFlot(form_get('num1'))
+let num2 = parseFlot(form_get('num2'))
+let num3 = parseFlot(form_get('num3'))
+
+console.log(num1, num2, num3)
+
+let media = parseFloat(num1 + num2 + num3) /  parseFloat(3)
+
+
+divResultado.innertHTML = `A media do s numero ${media}`
+
+})
