0918f19cf1212441466964f47854e7c9e96387e7
Miguel Rinaldo <miguel.r.nascimento9@aluno,senai.br>
2026-06-11 21:46:13 -0300

    adicionei muitos comandos script_questao02.js

--- js/script_questao01.js
+++ js/script_questao01.js
@@ -2,19 +2,21 @@ const formDados = document.querySelector('#formulario')
 const divResultado = document.querySelector('#resultado')
 
 //CAPTURANDO O EVENTO DE SUBMIT
-formDados.addEventListener('submit' , (evt) =>  {
+formDados.addEventListener('submit', (evt) => {
+    evt.preventDefault()
 
-const form_num = nem FormData(formDados)
+    const form_num = new FormData(formDados)
 
-let num1 = parseFlot(form_get('num1'))
-let num2 = parseFlot(form_get('num2'))
-let num3 = parseFlot(form_get('num3'))
+    let num1 = parseFloat(form_num.get('num1'))
+    let num2 = parseFloat(form_num.get('num2'))
+    let num3 = parseFloat(form_num.get('num3'))
 
-console.log(num1, num2, num3)
+    console.log(num1, num2, num3)
 
-let media = parseFloat(num1 + num2 + num3) /  parseFloat(3)
+    let media = parseFloat(num1 + num2 + num3) / parseFloat(3)
 
+    console.log(media)
 
-divResultado.innertHTML = `A media do s numero ${media,toFixed(2).replace('-', ',' ',')}`
+    divResultado.innerHTML = `A media do s numero ${media.toFixed(2).replace('.', ',')}`
 
 })
